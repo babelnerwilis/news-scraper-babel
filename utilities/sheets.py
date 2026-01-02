ade6369251785863a4eef083c62cefd0f7232038
babelnerwilis <babelnerwilis@gmail.com>
2026-01-02 09:47:43 +0700

    Update sheets.py

--- utilities/sheets.py
+++ utilities/sheets.py
@@ -1,1 +1,53 @@
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+FIELDNAMES = [
+    "day",
+    "publication_datetime",
+    "category",
+    "title",
+    "url",
+    "tags",
+    "total_pages",
+    "content"
+]
+
+def get_worksheet(spreadsheet_name, worksheet_name):
+    creds_json = os.environ["GOOGLE_SERVICE_JSON"]
+    creds_dict = json.loads(creds_json)
+
+    creds = Credentials.from_service_account_info(
+        creds_dict,
+        scopes=SCOPES
+    )
+
+    gc = gspread.authorize(creds)
+    sh = gc.open(spreadsheet_name)
+
+    try:
+        ws = sh.worksheet(worksheet_name)
+    except gspread.WorksheetNotFound:
+        ws = sh.add_worksheet(title=worksheet_name, rows=1, cols=len(FIELDNAMES))
+        ws.append_row(FIELDNAMES)
+
+    return ws
+
+def get_existing_urls(ws):
+    try:
+        return set(ws.col_values(5)[1:])  # url column
+    except Exception:
+        return set()
+
+def append_row(ws, row_dict):
+    ws.append_row(
+        [row_dict[f] for f in FIELDNAMES],
+        value_input_option="RAW"
+    )
 
