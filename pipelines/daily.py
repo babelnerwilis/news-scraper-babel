7bf8e217f1bca7eee219d9e4074a44b89e42ee15
babelnerwilis <babelnerwilis@gmail.com>
2026-01-02 15:52:16 +0700

    Update daily.py with 3 news limit

--- pipelines/daily.py
+++ pipelines/daily.py
@@ -1,57 +1,89 @@
 import time
 import random
+from datetime import datetime
 
 from scrapers.tribunnews import (
-    load_sitemap_articles,
-    extract_article_content
+    load_articles_from_sitemap,
+    extract_article_content,
 )
-from utilities.playwright_utils import get_page
+
+from utilities.playwright_utils import launch_browser
 from utilities.sheets import (
     get_worksheet,
-    get_existing_urls,
-    append_row
+    append_rows,
 )
+
 from config.settings import (
-    SPREADSHEET_NAME,
-    WORKSHEET_NAME,
-    CRAWL_DELAY
+    HEADERS,
+    SPREADSHEET_ID,
+    SHEET_NAME,
+    GOOGLE_CREDENTIALS_FILE,
 )
 
-def run():
-    ws = get_worksheet(SPREADSHEET_NAME, WORKSHEET_NAME)
-    existing_urls = get_existing_urls(ws)
 
-    articles = load_sitemap_articles()
+FIELDNAMES = [
+    "day",
+    "publication_datetime",
+    "source",
+    "title",
+    "url",
+    "tags",
+    "total_pages",
+    "content",
+]
+
+
+def run_daily():
+    articles = load_articles_from_sitemap()
+
     if not articles:
-        print("No new articles found.")
+        print("No articles found for today.")
         return
+    
+    # LIMIT FOR TESTING
+    articles = articles[:3]
+    print("Limit testing 3 articles")
 
-    p, browser, page = get_page()
+    worksheet = get_worksheet(
+        spreadsheet_id=SPREADSHEET_ID,
+        worksheet_name=SHEET_NAME,
+        credentials_file=GOOGLE_CREDENTIALS_FILE,
+    )
+
+    p, browser, page = launch_browser(HEADERS["User-Agent"])
+    results = []
 
     try:
         for i, art in enumerate(articles, 1):
-            if art["url"] in existing_urls:
-                continue
-
             print(f"[{i}/{len(articles)}] {art['url']}")
 
             try:
-                content, total_pages = extract_article_content(page, art["url"])
+                content, total_pages = extract_article_content(
+                    page, art["url"]
+                )
             except Exception as e:
                 content, total_pages = f"ERROR: {e}", 1
 
-            append_row(ws, {
+            results.append({
                 **art,
                 "total_pages": total_pages,
-                "content": content
+                "content": content,
             })
 
-            # time.sleep(CRAWL_DELAY)
-            MIN_DELAY = CRAWL_DELAY - 1
-            MAX_DELAY = CRAWL_DELAY + 1
-            time.sleep(random.uniform(MIN_DELAY, MAX_DELAY))
+            time.sleep(random.uniform(5, 7))
 
     finally:
         browser.close()
         p.stop()
 
+    append_rows(
+        worksheet=worksheet,
+        rows=results,
+        header=FIELDNAMES,
+    )
+
+    print(f"\nUpdated {len(results)} rows to Google Sheets")
+
+
+if __name__ == "__main__":
+    run_daily()
