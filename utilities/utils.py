b77d06cdb479e7938977026efd15e783510cb4df
babelnerwilis <babelnerwilis@gmail.com>
2026-01-02 09:48:01 +0700

    Update utils.py

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,1 +1,13 @@
+import re
+from datetime import datetime
+
+def extract_category_from_url(url):
+    m = re.search(r"tribunnews\.com/([^/]+)/", url)
+    return m.group(1) if m else ""
+
+def format_day(dt):
+    return dt.strftime("%d/%m/%Y")
+
+def format_datetime(dt):
+    return dt.strftime("%d/%m/%Y %H:%M")
 
